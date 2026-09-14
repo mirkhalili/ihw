e5f65b8e157f1ae84af36738c559389a3eaa37af
mirkhalili <mirkhalil.i@gmail.com>
2026-09-14 11:32:47 +0330

    db chnge

--- config/config.php
+++ config/config.php
@@ -8,9 +8,9 @@ return [
         'timezone' => 'Asia/Tehran',
     ],
     'db' => [
-        'dsn' => getenv('IHW_DB_DSN') ?: 'mysql:host=127.0.0.1;dbname=personnel;charset=utf8mb4',
-        'user' => getenv('IHW_DB_USER') ?: 'root',
-        'pass' => getenv('IHW_DB_PASS') ?: '',
+        'dsn' => getenv('IHW_DB_DSN') ?: 'mysql:host=127.0.0.1;dbname=ihw;charset=utf8mb4',
+        'user' => getenv('IHW_DB_USER') ?: 'ihw',
+        'pass' => getenv('IHW_DB_PASS') ?: 'ihw@1405',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
