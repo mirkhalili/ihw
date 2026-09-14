e097c696fc12cc837fc947b8a30ec0e1a72dc7c2
mirkhalili <mirkhalil.i@gmail.com>
2026-09-14 10:39:32 +0330

    Add upgrade migration for existing installations

---
+++ database/migration_001_hardening.sql
@@ -0,0 +1,7 @@
+USE personnel;
+
+-- Run this once when upgrading an existing installation created from an older schema.
+-- New installations should use schema.sql instead.
+ALTER TABLE personnel ADD INDEX idx_personnel_national_id (national_id);
+ALTER TABLE assets ADD INDEX idx_assets_status (status), ADD INDEX idx_assets_updated (updated_at);
+ALTER TABLE audit_logs ADD INDEX idx_audit_user (user_id);
